d7eeca2983143ad238043a7d3caaf83e1f49c0c9
Angie Spears <angie.spears809@gmail.com>
2020-09-30 22:19:33 -0400

    Add valid subsequence problem description

---
+++ algos/easyAlgos/ValidSubsequence.js
@@ -0,0 +1,14 @@
+// VALIDATE SUBSEQUENCE
+
+/*
+Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
+
+A subsequence of an array is a set of numbers that aren't necessarily adjancet in the array but that are in the same order as they appear in the array. For instance, the numbers [1,2,4] form a subsequence of the array [1,2,3,4], and so do the numbers [2, 4]. Note that a single number in an array and the array itself are both valid subsequences of the array.
+
+Sample input:
+array = [5,1,22,25,6,-1,8,100]
+sequence = [1,6,-1,10]
+
+Sample output:
+true
+*/
