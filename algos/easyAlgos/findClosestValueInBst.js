8863b6bb90412cadc320bf56d531b2e9ad8ae6e6
Angie Spears <angie.spears809@gmail.com>
2020-10-15 11:46:16 -0400

    Add findClosestValueBST description

---
+++ algos/easyAlgos/findClosestValueInBst.js
@@ -0,0 +1,17 @@
+// FIND CLOSEST VALUE IN BST
+
+/** Write a function that takes in a Binary Search Tree (BST) and a Target Intenger value and returns the closes value to that target value contained in the BST.
+ *
+ * You can assume that there will only be one closest value.
+ *
+ * Each BST node has an integer value, a left child node and a right child node. A node is said to be a valid BST node if and only if it satisfies the BST property: its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its
+ right; and its children nodes are either valid BST nodes themsevels or None/null.
+ *
+ * tree =  10
+ *     5        15
+ *   2   5    13  22
+ * 1         14
+ * Target = 12
+ */
+
+
